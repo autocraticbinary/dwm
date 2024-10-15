7d6ac8c5119de6955573f0584d6187ec132b482f
Autocratic Binary <autocraticbinary@gmail.com>
2024-10-16 00:49:50 +0530

    add togglestcky

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -18,13 +18,13 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10", "JoyPixels:pixelsize=11:antialias=true:autohint=true" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#A80030";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -39,11 +39,16 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class     					instance  title           	tags mask   isfloating  isterminal   noswallow  monitor */
+	{ "Gimp",    					NULL,     NULL,           	0,     		1,			0,           0,        -1 },
+	{ "Firefox", 					NULL,     NULL,           	1 << 8,		0,			0,          -1,        -1 },
+	{ "Alacritty",      			NULL,     NULL,           	0,     		0,			1,           0,        -1 },
+	{ NULL,      					NULL,     "Event Tester", 	0,     		0,			0,           1,        -1 }, /* xev */
+	{ "discord",  					NULL,	  NULL,	    		1 << 6,		0,			0,			 0,		   -1 },
+	{ "Brave-browser",  			NULL,	  NULL,	    		1 << 7,		0,			0,			 0,		   -1 },
+	{ "install4j-burp-StartBurp",   NULL,     NULL,       		1 << 8,		0,			0,			 0,        -1 },
+	{ "Virt-manager",   			NULL,     NULL,       		1 << 8,		0,			0,			 0,        -1 },
+	{ NULL, 	 					NULL,     "video0 - mpv", 	0,			1,          0,          -1,        -1 },
 };
 
 /* layout(s) */
@@ -88,7 +93,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 #include "movestack.c"
 
@@ -96,6 +101,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,						XK_s,      togglesticky,   {0} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
