8c79579ea2f7dd2eda06d09ca26f4d0612b20c77
Autocratic Binary <autocraticbinary@gmail.com>
2024-11-19 03:46:10 +0530

    add rules

--- config.h
+++ config.h
@@ -39,11 +39,17 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class     					instance  title           	tags mask  	isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    					NULL,     NULL,           	0,         	1,          0,           0,        -1 },
+	{ "Firefox", 					NULL,     NULL,           	1 << 8,    	0,          0,          -1,        -1 },
+	{ "St",      					NULL,     NULL,           	0,         	0,          1,           0,        -1 },
+	{ NULL,      					NULL,     "Event Tester", 	0,         	0,          0,           1,        -1 }, /* xev */
+	{ "discord",  					NULL,	  NULL,	    		1 << 6,		0,			0,			 0,		   -1 },
+	{ "Telegram",  					NULL,	  NULL,	    		1 << 6,		0,			0,			 0,		   -1 },
+	{ "Brave-browser",  			NULL,	  NULL,	    		1 << 7,		0,			0,			 0,		   -1 },
+	{ "install4j-burp-StartBurp",   NULL,     NULL,       		1 << 8,		0,			0,			 0,        -1 },
+	{ "Virt-manager",   			NULL,     NULL,       		1 << 8,		0,			0,			 0,        -1 },
+	{ NULL, 	 					NULL,     "video0 - mpv", 	0,			1,          0,          -1,        -1 },
 };
 
 /* layout(s) */
